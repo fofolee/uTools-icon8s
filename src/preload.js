3c9742b2221efe0dd2fea4bd7c948d0802fcdbc3
fofolee <fofolee@qq.com>
2024-01-05 23:44:37 +0800

    修改图标分类规则，增加默认图标分类的图标种类

--- src/preload.js
+++ src/preload.js
@@ -9,6 +9,16 @@ const path = require('path')
 
 const icons8 = {}
 
+const styleGroups = {
+    colored: 'office16,office30,office40,office80,color,fluent,nolan,3d-fluency,isometric,emoji,dusk,stickers,plasticine,bubbles,cotton,clouds,doodle,arcade,pulsar-color,clr-gls,ultraviolet,flat_round,parakeet,neon,matisse,tiny-color,stencil,avantgarde,water-color,retro,3d-plastilina,stitch',
+    ui: 'ios11,ios_filled,ios7,androidL,m_outlined,m_rounded,m_sharp,m_two_tone,fluent,fluent-systems-filled,fluent-systems-regular,win8,win10,sf-ultralight,sf-ultralight-filled,sf-regular,sf-black-filled,sf-black,sf-regular-filled,office16,office30,office40,office80,tiny-glyph,tiny-color,puffy,puffy-filled,color,3d-fluency,emoji,glyph-neue,p1em,ultraviolet,android',
+    outlined: 'fluent-systems-regular,win10,sf-ultralight,sf-regular,sf-black,ios7,m_outlined,dotty,Dusk_Wired,isometric-line,pastel_glyph,carbon_copy,pulsar-line,puffy,parakeet-line,badges,quill,laces,neon,hatch,water-color,pixels',
+    filled: 'ios_filled,ios11,androidL,m_rounded,m_sharp,fluent-systems-filled,win8,sf-ultralight-filled,sf-regular-filled,sf-black-filled,glyph-neue,puffy-filled,tiny-glyph,android',
+    _3d: 'isometric,isometric-line,3d-fluency,3d-plastilina',
+    handDrawn: 'doodle,quill,laces,matisse',
+    all: 'all'
+}
+
 // 更新数据库
 const pushData = (databases, data) => {
     var db = utools.db.get(databases);
@@ -154,18 +164,18 @@ const saveImg = (options, content, coding) => {
 
 // 偏好设置
 const showPreferences = async () => {
-    var language, platform, amount;
-    if (icons8.preferences == undefined) {
+    var language, platform, amount, style;
+    if (!icons8.preferences) {
         // language = "en-US";
-        platform = "color";
+        style = "colored";
         amount = "300";
         token = ""
     } else {
         // language = icons8.preferences.language;
-        platform = icons8.preferences.platform;
         size = icons8.preferences.size;
         amount = icons8.preferences.amount;
         token = icons8.preferences.token || "";
+        style = icons8.preferences.style || "colored"
     }
     utools.setExpendHeight(480)
     utools.subInputBlur();
@@ -173,15 +183,15 @@ const showPreferences = async () => {
         title: '设置',
         onBeforeOpen: () => {
             // document.getElementById('language').value = language;
-            document.getElementById('platform').value = platform;
+            document.getElementById('style').value = style;
         },
         backdrop: '#bbb',
         html:
-        `<!--搜索语言 <select id="language"class="swal2-select" style="width: 80%; height: 3rem; text-align: center; text-align-last: center">
+            `<!--搜索语言 <select id="language"class="swal2-select" style="width: 80%; height: 3rem; text-align: center; text-align-last: center">
             <option value="en-US">英文</>
             <option value="zh-CN">中文</>
         </select>-->
-        图标风格 <select id="platform" class="swal2-select" style="width: 80%; height: 3rem; text-align: center; text-align-last: center"> <option value="all">all</> <option value="color">color</> <option value="win8">win8</> <option value="win10">win10</> <option value="ios7">ios7</> <option value="android">android</> <option value="androidL">androidL</> <option value="office">office</> <option value="ultraviolet">ultraviolet</> <option value="nolan">nolan</> <option value="p1em">p1em</> <option value="dotty">dotty</> <option value="dusk">dusk</> <option value="Dusk_Wired">Dusk_Wired</> <option value="cotton">cotton</> <option value="ios11">ios11</> <option value="clouds">clouds</> <option value="bubbles">bubbles</> <option value="plasticine">plasticine</> <option value="carbon_copy">carbon_copy</> <option value="doodle">doodle</> <option value="fineline">fineline</> <option value="isometric">isometric</> <option value="flat_round">flat_round</> <option value="m_outlined">m_outlined</> <option value="m_rounded">m_rounded</> <option value="m_two_tone">m_two_tone</> <option value="m_sharp">m_sharp</> </select>
+        图标风格 <select id="style" class="swal2-select" style="width: 80%; height: 3rem; text-align: center; text-align-last: center"> <option value="all">全部</> <option value="colored">彩色</><option value="ui">UI</><option value="outlined">线条</><option value="filled">填充</><option value="_3d">3D</><option value="handDrawn">手绘</> </select>
         搜索数量 <input value="${amount}" placeholder="搜索图标的最大数量" id="amount" class="swal2-input" style="width: 80%; height: 3rem; text-align: center">
         APIKEY <input value="${token}" placeholder="APIKEY" id="token" class="swal2-input" style="width: 80%; height: 3rem; text-align: center">`,
         focusConfirm: false,
@@ -189,7 +199,7 @@ const showPreferences = async () => {
         preConfirm: () => {
             var data = {
                 // language: document.getElementById('language').value,
-                platform: document.getElementById('platform').value,
+                style: document.getElementById('style').value,
                 amount: document.getElementById('amount').value,
                 token: document.getElementById('token').value
             }
@@ -211,7 +221,7 @@ const showMoreFeatures = async () => {
     await swal.fire({
         backdrop: '#bbb',
         html:
-        `选择图标尺寸 <select id="size" class="swal2-select" style="width: 80%; height: 3rem; margin: 5px; text-align: center; text-align-last: center"> <option value="1x">1x</> <option value="2x">2x</> <option value="3x">3x</> <option value="4x">4x</> <option value="5x">5x</> <option value="6x">6x</> <option value="7x">7x</> <option value="8x">8x</> <option value="9x">9x</> <option value="10x">10x</> <option value="11x">11x</> <option value="12x">12x</> </select>
+            `选择图标尺寸 <select id="size" class="swal2-select" style="width: 80%; height: 3rem; margin: 5px; text-align: center; text-align-last: center"> <option value="1x">1x</> <option value="2x">2x</> <option value="3x">3x</> <option value="4x">4x</> <option value="5x">5x</> <option value="6x">6x</> <option value="7x">7x</> <option value="8x">8x</> <option value="9x">9x</> <option value="10x">10x</> <option value="11x">11x</> <option value="12x">12x</> </select>
         <button id="png" class="swal2-confirm swal2-styled" style="width: 80%; height: 3rem; margin: 5px">下载 PNG</button>
         <button id="svg" class="swal2-confirm swal2-styled" style="width: 80%; height: 3rem; margin: 5px">下载 SVG</button>
         <button id="ico" class="swal2-confirm swal2-styled" style="width: 80%; height: 3rem; margin: 5px">转为 ICO</button>
@@ -259,10 +269,10 @@ const showMoreFeatures = async () => {
 // 搜索
 const search = async searchWord => {
     var amount = icons8.preferences.amount;
-    var platform = icons8.preferences.platform;
+    var style = icons8.preferences.style;
     var token = icons8.preferences.token;
     var language = /[\u4e00-\u9fa5]/.test(searchWord) ? 'zh' : 'en';
-    var url = `https://search.icons8.com/api/iconsets/v5/search?term=${encodeURIComponent(searchWord)}&amount=${amount}&offset=0&platform=${platform}&language=${language}`;
+    var url = `https://search.icons8.com/api/iconsets/v5/search?term=${encodeURIComponent(searchWord)}&amount=${amount}&offset=0&platform=${styleGroups[style]}&language=${language}`;
     if (token) url += `&token=${token}`
     var data = await get(url);
     var icons = JSON.parse(data).icons
@@ -282,6 +292,7 @@ window.exports = {
             enter: async (action, callbackSetList) => {
                 try {
                     icons8.preferences = utools.db.get("icons8Preferences").data;
+                    if (!icons8.preferences.style) icons8.preferences.style = 'colored'
                 } catch (error) {
                     showPreferences()
                 }
